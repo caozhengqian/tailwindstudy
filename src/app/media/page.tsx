f03aacd40a7ddee2e1279b02bc22654256959a21
caoxiansheng <1264419544@qq.com>
2025-08-14 00:07:29 +0800

    bug

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -3,10 +3,10 @@ export default function Media() {
 
     return (
         <>
-            <div className="sm:bg-amber-500 md:bg-amber-700 text-">
+            <div className="sm:bg-amber-500 md:bg-amber-700">
                 <p >sm(大于640px)md(大于768px)</p>
             </div>
-            <div className="max-sm:bg-amber-500 max-md:bg-amber-700 text-">
+            <div className="max-sm:bg-amber-500 max-md:bg-amber-700">
                 <p >max-sm(小于640px)max-md(小于768px)</p>
             </div>
         </>
