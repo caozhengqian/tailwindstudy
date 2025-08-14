5fbba75b39b53e85cedd011b51b33ab56a120223
caoxiansheng <1264419544@qq.com>
2025-08-14 13:22:41 +0800

    8.方法：比如open

---
+++ src/app/fun/page.tsx
@@ -0,0 +1,14 @@
+export default function Fun() {
+
+    return (
+        <details className="border border-transparent open:border-black/10 open:bg-gray-100 " open>
+            <summary className="text-sm leading-6 font-semibold text-gray-900 select-none">Why do they call it
+                Ovaltine?
+            </summary>
+            <div className="mt-3 text-sm leading-6 text-gray-600">
+                <p>The mug is round. The jar is round. They should call it Roundtine.</p>
+            </div>
+        </details>
+
+    );
+}
