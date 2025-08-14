71967601d091ac761684304d899ca748bed9cf0d
caoxiansheng <1264419544@qq.com>
2025-08-14 13:23:22 +0800

    9.交互：比如输入光标为粉红色

---
+++ src/app/active/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Active() {
+
+    return (
+        <>
+            <div className="m-5">
+                <textarea className="caret-pink-500"></textarea>
+            </div>
+        </>
+
+    );
+}
